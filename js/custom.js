f2259ca8415f5979055b130be6bc42566d8784de
pankaj kumar <pankajkumar.15467@yahoo.co.in>
2019-04-15 22:03:47 +0530

    new

--- js/custom.js
+++ js/custom.js
@@ -159,5 +159,18 @@
     });
 
 
+ 
+        $(".testimonial-carousel").slick({
+          infinite: true,
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          autoplay: false,
+          arrows: true,
+          prevArrow: $(".testimonial-carousel-controls .prev"),
+          nextArrow: $(".testimonial-carousel-controls .next")
+        });
+      
+      
+
 
 })(jQuery);
